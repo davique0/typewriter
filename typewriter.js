9e0687df3ae8218b078fbdb15dee486e60150316
davique0 <davique@gmail.com>
2022-11-14 10:13:25 -0500

    typewriter file working with a loop and a cb function

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,60 @@
-console.log('HELLO');
+const sentence = "hello"; 
+
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(printer(printer(sentence[i])))
+  }, i * 500)
+}
+
+
+const printer = (char) => {
+  
+    return char
+  
+};
+
+
+// const timer = (str) => {
+//   for (const char of str) {
+//     process.stdout.write(printer(char));
+//   }
+// }
+
+// timer(sentence);
+
+// for (const item of sentence){
+// setTimeout(() => {
+//   for (const char of sentence) {
+//     process.stdout.write(char);
+//   }
+// }, 50)
+// }
+
+// const typewriter = (str, cb) => {
+  // for (const char of str) {
+  //   cb(char);
+  // }
+// }
+
+// typewriter(sentence, char => setTimeout(() => {
+//      process.stdout.write(char)
+//   }, 500,));
+
+// while (i < sentence.length) {
+//   setTimeout(() => {
+//     process.stdout.write(sentence[i])
+//     i++
+//   }, 50)
+// }
+
+// for (let i = 0; i < sentence.length; i++) {
+//   setTimeout(() => {
+//     process.stdout.write(sentence[i])
+//   }, 50)
+// };
+
+// for (const char of sentence) {
+//   setTimeout(() => {
+//     process.stdout.write(char)
+//   }, 500)
+// };
